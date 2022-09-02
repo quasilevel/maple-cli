5348b87e5efc4553e2f386ebbab922631e5fc32f
keogami <41939011+keogami@users.noreply.github.com>
2022-09-02 20:08:46 +0530

    got the subcommands working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct Args {
+   #[clap(subcommand)]
+   action: Action,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Action {
+   Add,
+   Play,
+   Find,
+   Sync
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?} selected", args.action);
 }
