5f7dd5dadaa9fb87bfa36fd48feac80f4ffee4fd
keogami <41939011+keogami@users.noreply.github.com>
2022-09-07 01:58:44 +0530

    created a better tag input mechanic

--- src/add.rs
+++ src/add.rs
@@ -10,21 +10,21 @@ pub fn add() -> Result<(), Box<dyn Error>> {
     let name = Text::new("Name:").prompt()?;
     let url = CustomType::<Url>::new("Url:").prompt()?;
     let description = Editor::new("Description:").prompt()?;
-    let tags = input_tags()?;
+    let tags = input_tags(&["jazz", "english", "japanese"])?;
     println!("{} {} {} {:?}", name, url, description, tags);
     Ok(())
 }
 
-fn input_tags() -> Result<Vec<String>, Box<dyn Error>> {
+fn input_tags(suggestions: &[&str]) -> Result<Vec<String>, Box<dyn Error>> {
     let mut tags: Vec<String> = Vec::new();
-    add_tags(&mut tags)?;
+    add_tags(&mut tags, suggestions)?;
 
     loop {
         let options: Vec<&str> = vec![ADD_TAGS, REMOVE_TAGS, DONE];
         let opt = Select::new("Choose an option", options).prompt()?;
 
         if opt == ADD_TAGS {
-            add_tags(&mut tags)?;
+            add_tags(&mut tags, suggestions)?;
         } else if opt == REMOVE_TAGS {
             remove_tags(&mut tags)?;
         } else {
@@ -35,14 +35,26 @@ fn input_tags() -> Result<Vec<String>, Box<dyn Error>> {
     Ok(tags)
 }
 
-fn add_tags(tags: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
-    loop {
-        let val = Text::new("Tag:").with_help_message("Press enter to stop").prompt()?;
-        if val.is_empty() {
-            break;
+fn add_tags(tags: &mut Vec<String>, suggestions: &[&str]) -> Result<(), Box<dyn Error>> {
+    let suggester = |input: &str| -> Result<Vec<String>, inquire::CustomUserError> {
+        let list: Vec<&str> = input.split(',').map(|x| x.trim()).collect();
+        let (last, rest_vec) = list.split_last().unwrap_or((&"", &[""]));
+        let mut rest = rest_vec.join(", ");
+
+        if !rest.is_empty() {
+            rest += ", ";
         }
-        tags.push(val)
-    }
+
+        Ok(suggestions.iter()
+            .filter(|&val| !rest_vec.contains(val) && val.contains(&last.to_ascii_lowercase()))
+            .map(|&val| format!("{}{}", rest.to_owned(), val))
+            .collect())
+            
+    };
+
+    let text = Text::new("Tags:").with_help_message("Enter a comma separated list of tags").with_suggester(&suggester).prompt()?;
+
+    tags.append(&mut text.split(',').map(|val| val.trim().to_string()).collect());
     Ok(())
 }
 
