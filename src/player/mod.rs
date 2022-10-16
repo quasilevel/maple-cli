309306994eec0546e03c19774429f304baedeacb
keogami <41939011+keogami@users.noreply.github.com>
2022-10-16 11:34:48 +0530

    created the player abstraction

---
+++ src/player/mod.rs
@@ -0,0 +1,6 @@
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub trait Player: std::fmt::Display + Sized {
+    fn find_all() -> Vec<Self>;
+    fn queue(&self, url: String) -> Result<()>;
+}
